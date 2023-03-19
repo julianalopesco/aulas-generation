d9a90b8a10584d78b547ce175543e022283a9a1f
Julianalopesco <julianalopesmf@gmail.com>
2023-03-19 17:34:17 -0300

    exercicio matriz

--- Exercícios/src/vetorMatriz/matrizMediasNotas.java
+++ Exercícios/src/vetorMatriz/matrizMediasNotas.java
@@ -6,15 +6,33 @@ import java.util.Scanner;
 public class matrizMediasNotas {
 
 	public static void main(String[] args) {
-		int[][] notas = new int[4][10];
-		int participante=0, nota=0, mediaParticipante,i=-1;
+		int[][] notas = new int[10][4];
+		int participante = 0, nota = 0;
+		double mediaParticipante;
+		double[] medias = new double[10];
 
 		Scanner read = new Scanner(System.in);
+
+		for (participante = 0; participante < 10; participante++) {
+			for (nota = 0; nota < 4; nota++) {
+				System.out.println("\nEntre com a nota " + (nota + 1) + " do participante " + (participante + 1) + ": ");
+				notas[participante][nota] = read.nextInt();
+			}
+		}
 		
-		for(nota=0;participante<9;participante++) {
-			System.out.println("\nEntre com a " + i + "nota: ");
-			notas[participante][nota] = read.nextInt();
+		for (participante = 0; participante < 10; participante++) {
+			mediaParticipante = 0;
+			for (nota = 0; nota < 4; nota++) {
+				mediaParticipante += notas[participante][nota];
+			}
+			mediaParticipante /= 4;
+			medias[participante] = mediaParticipante;
+			System.out.println("Média do participante " + (participante + 1) + ": " + mediaParticipante);
 		}
 
+		System.out.println("Médias dos participantes:");
+		for (participante = 0; participante < 10; participante++) {
+			System.out.print("| "+medias[participante]);
+		}
 	}
 }
