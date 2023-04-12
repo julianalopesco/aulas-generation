721b27bf6984976bd946454c9e9274df94b920f7
Julianalopesco <julianalopesmf@gmail.com>
2023-04-12 16:17:57 -0300

    exercicio springboot

--- Exercícios banco de dados/helloworld/src/main/java/org/generation/helloworld/controller/HelloController.java
+++ Exercícios banco de dados/helloworld/src/main/java/org/generation/helloworld/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
+
 // indica para o spring que o codigo abaixo vai ser um controller
 @RestController
 
@@ -16,8 +18,21 @@ public class HelloController {
 		return "Hello World";
 	}
 	
-	@GetMapping("/olaMundo") //sub-rota
+	@GetMapping("/listaBsm") //sub-rota
 	public String olaMundo() {
-		return "Olá Mundo";
+		return  "BSMs Valorizadas pela Generation Brasil\n"
+				+ "Mentalidade de crescimento\n"
+				+ "Persistência\n"
+				+ "Responsabilidade Pessoal\n"
+				+ "Comunicação\n"
+				+ "Orientação ao detalhe\n"
+				+ "Proatividade\n"
+				+ "Trabalho em equipe\n";			
+	}
+	
+	@GetMapping("/meta")
+	public String olamundo() {
+		return "Minha meta da semana:\n"
+				+"Essa semana, minha meta é me desenvolver melhor em Trabalho em Equipe\n";
 	}
 }
